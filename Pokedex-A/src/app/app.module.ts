d3c97a5f383ef1012b5df0d253b1dd58dc117231
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-07-12 17:09:22 -0300

    Configurando componentes PokeHeader, PokeList e PokeSearch.

--- Pokedex-A/src/app/app.module.ts
+++ Pokedex-A/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-
+// Components
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
---
+++ Pokedex-A/src/app/shared/poke-search/poke-search.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poke-search',
+  templateUrl: './poke-search.component.html',
+  styleUrls: ['./poke-search.component.css']
+})
+export class PokeSearchComponent {
+
+}
