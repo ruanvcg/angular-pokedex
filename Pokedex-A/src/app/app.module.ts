9d7b08d68d20be9a2d0bade139bb6b9d90eab955
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-07-17 20:25:20 -0300

    Configurando as rotas para os detalhes dos pokemons.

--- Pokedex-A/src/app/app.module.ts
+++ Pokedex-A/src/app/app.module.ts
@@ -4,9 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-
-// Components
 import { PagesModule } from './pages/pages.module';
+
+
 
 @NgModule({
   declarations: [
