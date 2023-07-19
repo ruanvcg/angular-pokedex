4d3e91942a30ef6aaaab492f8ad6053a00af3d2c
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-07-19 18:26:23 -0300

    Bug fix - Carregamento das informações individuais.

--- Pokedex-A/src/app/pages/details/details.component.ts
+++ Pokedex-A/src/app/pages/details/details.component.ts
@@ -29,7 +29,7 @@ export class DetailsComponent {
     this.getPokemon;
   }
 
-  get getPokemon(){
+  public getPokemon(){
     const id = this.activatedRoute.snapshot.params['id'];
     const pokemon = this.pokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
     const name = this.pokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
